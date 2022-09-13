9b8f3a53e39ecbfd40d05d5d14e55cf3e80486e2
laiqueshaikh <113412676+laiqueshaikh@users.noreply.github.com>
2022-09-13 17:51:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit .header ('🍌🥭 My Mom's New Healthy Diner 🥝🍇')
+
 streamlit.header ('Breakfast Favorites')
 
 streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
